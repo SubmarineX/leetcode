5f1630ad3a9184471a0c6abdf7a5bc49841b838d
Shen Zhiqiang <shen.zhiqiang@hotmail.com>
2024-07-21 16:17:41 +0800

    [Problems/Algorithms] Added 287. Find the Duplicate Number

--- README.md
+++ README.md
@@ -16,6 +16,7 @@
 | 88 | [Merge Sorted Array](https://leetcode.com/problems/merge-sorted-array/) | [C++](./algorithms/merge-sorted-array/merge-sorted-array.cc) | Easy | Array, Two Pointers, Sorting |
 | 167 | [Two Sum II - Input Array Is Sorted](https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/) | [C++](./algorithms/two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cc) | Medium | Array, Two Pointers, Binary Search |
 | 189 | [Rotate Array](https://leetcode.com/problems/rotate-array/) | [C++](./algorithms/rotate-array/rotate-array.cc) | Medium | Array, ath, Two Pointers |
+| 287 | [Find the Duplicate Number](https://leetcode.com/problems/find-the-duplicate-number/) | [C++](./algorithms/find-the-duplicate-number/find-the-duplicate-number.cc) | Medium | Array, Two Pointers, Binary Search, Bit Manipulation |
 | 283 | [Move Zeroes](https://leetcode.com/problems/move-zeroes/) | [C++](./algorithms/move-zeroes/move-zeroes.cc) | Easy | Array, Two Pointers |
 | 349 | [Intersection of Two Arrays](https://leetcode.com/problems/intersection-of-two-arrays/) | [C++](./algorithms/intersection-of-two-arrays/intersection-of-two-arrays.cc) | Easy | Array, Hash Table, Two Pointers, Binary Search, Sorting |
 | 350 | [Intersection of Two Arrays II](https://leetcode.com/problems/intersection-of-two-arrays-ii/) | [C++](./algorithms/intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cc) | Easy | Array, Hash Table, Two Pointers, Binary Search, Sorting |
---
+++ algorithms/find-the-duplicate-number/find-the-duplicate-number.cc
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+/*
+ * Given an array of integers nums containing n + 1 integers where each integer
+ * is in the range [1, n] inclusive.
+ *
+ * There is only one repeated number in nums, return this repeated number.
+ *
+ * You must solve the problem without modifying the array nums and uses only
+ * constant extra space.
+ */
+
+class Solution {
+ public:
+  /**
+   * Floyd's Tortoise and Hare (Cycle Detection) algorithm
+   * (or slow and fast pointer). This algorithm is typically used for detecting
+   * cycles in a linked list but can be adapted for this problem since the
+   * presence of a duplicate number implies a cycle in the "linked list" formed
+   * by treating array indices as pointers.
+   */
+  int findDuplicate(vector<int>& nums) {
+    int tortoise = nums[0];
+    int hare = nums[0];
+    while (true) {
+      tortoise = nums[tortoise];
+      hare = nums[nums[hare]];
+      if (tortoise == hare) break;
+    }
+
+    tortoise = nums[0];
+    while (tortoise != hare) {
+      tortoise = nums[tortoise];
+      hare = nums[hare];
+    }
+
+    return hare;
+  }
+
+  /**
+   * Binary Search. The core idea of the binary search method is to utilize the
+   * relationship between elements and indices in the array. By counting the
+   * number of elements less than or equal to a certain value, we can
+   * progressively narrow down the range that might contain the duplicate
+   * number.
+   */
+  int findDuplicate(vector<int>& nums) {
+    int low = 1;
+    int high = nums.size() - 1;
+
+    while (low < high) {
+      int mid = low + (high - low) / 2;
+      int count = std::count_if(nums.begin(), nums.end(),
+                                [&mid](int i) { return i <= mid; });
+      if (count <= mid) {
+        low = mid + 1;
+      } else {
+        high = mid;
+      }
+    }
+
+    return low;
+  }
+};
